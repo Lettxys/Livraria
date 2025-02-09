7cc04259843485e24b0170091d1c2a57bd088463
João Soares <jmkoygg@gmail.com>
2025-02-08 22:45:04 -0300

    Criando classe Usuario

---
+++ Usuarios/UsuarioServices.java
@@ -0,0 +1,12 @@
+package Livraria.Usuarios;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class UsuarioServices {
+
+    public void CriarUsuario(Usuario usuario) {
+        
+    }
+    
+}
